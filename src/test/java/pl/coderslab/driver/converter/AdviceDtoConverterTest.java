f857faa5f1c250abcba2d3b30d80d5c7d07692c3
ArtemKoliushko <koliushko.a@gmail.com>
2020-11-11 12:02:12 +0100

    Travis-ci tests

---
+++ src/test/java/pl/coderslab/driver/converter/AdviceDtoConverterTest.java
@@ -0,0 +1,14 @@
+package pl.coderslab.driver.converter;
+
+import org.junit.jupiter.api.Test;
+
+class AdviceDtoConverterTest {
+
+    @Test
+    void convertToAdviceDto() {
+    }
+
+    @Test
+    void convertToAdvice() {
+    }
+}
