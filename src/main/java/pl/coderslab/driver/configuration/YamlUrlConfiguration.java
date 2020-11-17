00bb7c442aa415a6f0c84427be47c8f3530954ae
ArtemKoliushko <koliushko.a@gmail.com>
2020-11-17 18:22:30 +0100

    AdviceControllerTest added YamlUrlConfiguration removed @EqualsAndHashCode Lombok annotation

--- src/main/java/pl/coderslab/driver/configuration/YamlUrlConfiguration.java
+++ src/main/java/pl/coderslab/driver/configuration/YamlUrlConfiguration.java
@@ -1,6 +1,5 @@
 package pl.coderslab.driver.configuration;
 
-import lombok.EqualsAndHashCode;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +10,6 @@ import pl.coderslab.driver.utils.YamlPropertySourceFactory;
 @ConfigurationProperties(prefix = "urls")
 @PropertySource(value = "classpath:urls.yaml", factory = YamlPropertySourceFactory.class)
 @Setter
-@EqualsAndHashCode
 public class YamlUrlConfiguration {
 
     private String mediaFileDownloadUrl;
